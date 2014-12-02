be136cd6c5d6b9f7708b243a0731d54ed23441f9
Ben Waine <ben@ben-waine.co.uk>
2014-12-02 00:16:18 +0000

    Added xhprof and xdebug

--- build/provision.sh
+++ build/provision.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
+
+# Obligatory Update
 sudo apt-get update
-sudo apt-get install -y apache2 php5 php5-curl
+
+# Install PHP5.6 + Apache 2
+sudo apt-get install -y apache2 php5 php5-dev php5-curl
 sudo cp /vagrant/build/toolbox.conf /etc/apache2/sites-available/toolbox-demo.conf
 sudo a2dissite 000-default.conf
 sudo a2ensite toolbox-demo.conf
 sudo a2enmod rewrite
+
+# Install XDEBUG
+sudo pecl install xdebug
+sudo cp /vagrant/build/xdebug.ini /etc/php5/mods-available/xdebug.ini
+sudo php5enmod xdebug
+
+# Install XHPROF
+sudo cp /vagrant/build/xhprof.so /usr/lib/php5/20121212/xhprof.so
+sudo cp /vagrant/build/xhprof.ini /etc/php5/mods-available/xhprof.ini
+sudo php5enmod xhprof
+
 sudo service apache2 restart
+
